95dcec7aac3ece0519ff99bc82eaa5677ffb8cca
Queiroz, Jayme <jaymedq@yahoo.com.br>
2025-08-23 10:21:12 -0300

    Add constants

---
+++ tools/constants.py
@@ -0,0 +1,3 @@
+import numpy as np
+RADAR_PLACEMENT = np.array([0.995, -7.88, 1.70])  # Radar position in room coordinates
+# RADAR_PLACEMENT = np.array([0.98, -4.5, 1.78])  # Alternative radar position
