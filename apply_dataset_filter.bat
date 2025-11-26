2e17f2c928305e885b09a3be67df55773cb2e3c6
Queiroz, Jayme <jaymedq@yahoo.com.br>
2025-11-26 00:19:50 -0300

    Rename BLE nov 10th experiment data

--- apply_dataset_filter.bat
+++ apply_dataset_filter.bat
@@ -1,4 +1,8 @@
-py tools\filter_dataset_columns.py --input "Results\T136_MMW_A5_BLE_C2P2\exported_T136_MMW_A5_BLE_C2P2.txt" --columns x y --threshold 0.2
-py tools\filter_dataset_columns.py --input "Results\T137_MMW_A5_BLE_C2P3\exported_T137_MMW_A5_BLE_C2P3.txt" --columns x y --threshold 0.2
-py tools\filter_dataset_columns.py --input "Results\T138_MMW_A5_BLE_C2P4\exported_T138_MMW_A5_BLE_C2P4.txt" --columns x y --threshold 0.2
-py tools\filter_dataset_columns.py --input "Results\T139_MMW_A5_BLE_C2P5\exported_T139_MMW_A5_BLE_C2P5.txt" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T136_MMW_A5_BLE_C2P2\T136_MMW_A5_BLE_C2P2_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T137_MMW_A5_BLE_C2P3\T137_MMW_A5_BLE_C2P3_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T138_MMW_A5_BLE_C2P4\T138_MMW_A5_BLE_C2P4_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T139_MMW_A5_BLE_C2P5\T139_MMW_A5_BLE_C2P5_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T146_MMW_A5_BLE_C3P2\T146_MMW_A5_BLE_C3P2_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T147_MMW_A5_BLE_C3P3\T147_MMW_A5_BLE_C3P3_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T148_MMW_A5_BLE_C3P4\T148_MMW_A5_BLE_C3P4_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T149_MMW_A5_BLE_C3P5\T149_MMW_A5_BLE_C3P5_ble_data.csv" --columns x y --threshold 0.2
