5ee57204e202e0835f0ef9c40b58c0c7fa58bcb9
Queiroz, Jayme <jaymedq@yahoo.com.br>
2025-12-02 07:26:28 -0300

    Update dataset bat to filter new data from nov 26

--- apply_dataset_filter.bat
+++ apply_dataset_filter.bat
@@ -6,3 +6,11 @@ py tools\filter_dataset_columns.py --input "Results\T146_MMW_A5_BLE_C3P2\T146_MM
 py tools\filter_dataset_columns.py --input "Results\T147_MMW_A5_BLE_C3P3\T147_MMW_A5_BLE_C3P3_ble_data.csv" --columns x y --threshold 0.2
 py tools\filter_dataset_columns.py --input "Results\T148_MMW_A5_BLE_C3P4\T148_MMW_A5_BLE_C3P4_ble_data.csv" --columns x y --threshold 0.2
 py tools\filter_dataset_columns.py --input "Results\T149_MMW_A5_BLE_C3P5\T149_MMW_A5_BLE_C3P5_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T153_MMW_A5_BLE_C3P2\T153_MMW_A5_BLE_C3P2_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T154_MMW_A5_BLE_C3P3\T154_MMW_A5_BLE_C3P3_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T155_MMW_A5_BLE_C3P4\T155_MMW_A5_BLE_C3P4_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T156_MMW_A5_BLE_C3P5\T156_MMW_A5_BLE_C3P5_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T157_MMW_A5_BLE_C2P2\T157_MMW_A5_BLE_C2P2_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T158_MMW_A5_BLE_C2P3\T158_MMW_A5_BLE_C2P3_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T159_MMW_A5_BLE_C2P4\T159_MMW_A5_BLE_C2P4_ble_data.csv" --columns x y --threshold 0.2
+py tools\filter_dataset_columns.py --input "Results\T160_MMW_A5_BLE_C2P5\T160_MMW_A5_BLE_C2P5_ble_data.csv" --columns x y --threshold 0.2
